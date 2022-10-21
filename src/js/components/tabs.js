66fed27763911c45f117b8ea11b32f5294f51aa5
Vitaly Reutsky <vreutsky77@mail.ru>
2022-10-21 17:13:33 +0300

    Добавил логики на страницу для каталога

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,10 +1,13 @@
 // Реализация табов
 const sectionProducts = document.querySelector(".products");
+const sectionProduct = document.querySelector(".single-product");
 
 let tabs = document.querySelectorAll(".product__item-title"),
   tabsContent = document.querySelectorAll(".tab-content"),
   tabsParent = document.querySelector(".tab-header__items"),
-  tabsMenu = document.querySelectorAll(".product__item-menu");
+  tabsMenu = document.querySelectorAll(".product__item-menu"),
+  categoryTitle = document.querySelector(".products__category-title"),
+  hash = window.location.hash.replace("#", "");
 
 if (sectionProducts) {
   function hideTabContent() {
@@ -23,10 +26,68 @@ if (sectionProducts) {
     tabsContent[i].classList.remove("hide");
 
     tabs[i].classList.add("tab-header__item--active");
+
+    let btnClear = document.querySelector(".products__btn-clear");
+
+    btnClear.addEventListener("click", () => {
+      history.go(0);
+    });
+
+    categoryTitle.firstChild.textContent = tabs[i].firstChild.textContent;
+    categoryTitle.lastElementChild.textContent =
+      tabs[i].querySelector("span").textContent;
   }
 
   hideTabContent();
-  showTabContent();
+  if (tabs[hash]) {
+    showTabContent(hash);
+  } else {
+    showTabContent();
+  }
+
+  tabsParent.addEventListener("click", function (event) {
+    const target = event.target;
+    if (target.classList.contains("product__item-title")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+
+          categoryTitle.firstChild.textContent = target.firstChild.textContent;
+          categoryTitle.lastElementChild.textContent =
+            target.querySelector("span").textContent;
+        }
+      });
+    }
+  });
+}
+
+if (sectionProduct) {
+  function hideTabContent() {
+    tabs.forEach((item) => {
+      item.classList.remove("tab-header__item--active");
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabs[i].classList.add("tab-header__item--active");
+
+    categoryTitle.firstChild.textContent = tabs[i].firstChild.textContent;
+    categoryTitle.lastElementChild.textContent =
+      tabs[i].querySelector("span").textContent;
+  }
+
+  hideTabContent();
+  if (tabs[hash]) {
+    showTabContent(hash);
+  } else {
+    showTabContent();
+  }
+
+  let btnClear = document.querySelector(".products__btn-clear");
+  btnClear.addEventListener("click", () => {
+    history.go(0);
+  });
 
   tabsParent.addEventListener("click", function (event) {
     const target = event.target;
@@ -48,7 +109,7 @@ for (i = 0; i < itemMenuOpen.length; i++) {
   let thisitemMenuOpen = itemMenuOpen[i];
   let thisItemSvg = itemMenuOpen[i].querySelector("svg");
 
-  itemMenuOpen[i].addEventListener("click", function () {
+  itemMenuOpen[i].addEventListener("click", function (e) {
     subMenu.classList.toggle("show-tabs-menu");
     thisitemMenuOpen.classList.toggle("tab-header__item--active");
 
